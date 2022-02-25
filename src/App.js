1c549ffa99e74a2a382a36770585ccba455bac51
taniryla <ryland0000@gmail.com>
2022-02-24 18:31:33 -0700

    part 4 of the lab

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import SkillList from './SkillList';
 import NewSkillForm from './NewSkillForm';
 import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3 },
-  { name: "JavaScript", level: 4 },
-  { name: "Python", level: 2 },
-];
+
 
 function App() {
+  const [ skills, setSkill] = useState([
+    { name: "HTML", level: 5 },
+    { name: "CSS", level: 3 },
+    { name: "JavaScript", level: 4 },
+    { name: "Python", level: 2 },
+  ]);
+
+  function addSkill (newSkill) {
+    setSkill([newSkill, ...skills]);
+  }
+
   return (
     <div className="App">
       <h1>React Dev Skills</h1>
       <SkillList skills={skills}/>
       <hr />
-      <NewSkillForm />
+      <NewSkillForm addSkill={addSkill}/>
     </div>
   );
 }
--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,25 +1,41 @@
 import "./NewSkillForm.css"
+import { useState } from "react";
 
 
+export default function NewSkillForm( { addSkill }) {
+    const [formData, setFormData] = useState(
+        {name: "", level: 3 })
 
-export default function NewSkillForm() {
+    function handleChange (evt) {
+        setFormData({...formData, [evt.target.name]: evt.target.value})
+    }
+
+    function handleAddNewSkill (evt) {
+        evt.preventDefault();
+        addSkill(formData);
+        setFormData(
+            {name: "", level: 3 })
+      }
 
     return (
-        <form className="NewSkillForm">
+        <form className="NewSkillForm" onSubmit={handleAddNewSkill} >
             <div className="NewSkillFormDiv">
             <label>Skills</label>
-            <input></input>
-            <label>Level  
-                <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
+            <input name="name" 
+            value={formData.name}
+            onChange={handleChange}
+            ></input>
+            <label >Level  
+                <select name="level" value={formData.level} onChange={handleChange}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
                 </select>
             </label>
             </div>
-            <button>Add Skill</button>
+            <button type="submit">Add Skill</button>
         </form>    
     )
 }
